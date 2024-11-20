c10dbd9db3dd9f8a8c41ffba19026de6921aedd5
zziyinn <ziyinzx@gmail.com>
2024-11-19 21:06:49 -0500

    delete

--- fittrack/backend/models/Weight.js
+++
@@ -1,9 +0,0 @@
-// backend/models/Weight.js
-const mongoose = require('mongoose');
-
-const weightSchema = new mongoose.Schema({
-  date: { type: Date, default: Date.now },
-  weight: { type: Number, required: true }
-});
-
-module.exports = mongoose.model('Weight', weightSchema);
