37a63b267f1fcfdd53ebb4e5e2f12e882dba9f96
Anqi Zhou <114124278+aqiizzz@users.noreply.github.com>
2024-12-01 17:46:06 -0500

    update weight log and ai assisitant

--- backend/WorkoutAPI/index.js
+++ backend/WorkoutAPI/index.js
@@ -3,8 +3,10 @@ const app = express();
 const cors = require("cors"); // Import the CORS middleware
 const port = 8000;
 const exercises = require("./exercise-data.json");
+const fs = require('fs');
 
 app.use(cors()); // Enable CORS for all routes
+app.use(express.json());
 // Define API endpoint
 app.get("/random-workout", (req, res) => {
   const { muscleGroup, level, num } = req.query;
@@ -23,6 +25,79 @@ app.get("/random-workout", (req, res) => {
   // Limit the number of results
   const randomExercises = filteredExercises.slice(0, parseInt(num));
   res.json(randomExercises);
+});
+
+// Save weight data to local storage
+function saveWeightData(user, date, weight) {
+
+  let weightData = fs.readFileSync('weight-data.json', 'utf8');
+  weightData = JSON.parse(weightData);
+    
+  const existingEntryIndex = weightData[user]['weightData'].findIndex((entry) => entry.date === date);
+  
+  if (existingEntryIndex !== -1) {
+    weightData[user]['weightData'][existingEntryIndex].weight = weight;
+  } else {
+    weightData[user]['weightData'].push({ date: date, weight: weight });
+  }
+
+  weightData[user]['weightData'].sort((a, b) => new Date(a.date) - new Date(b.date));
+  fs.writeFileSync('weight-data.json', JSON.stringify(weightData));
+  return weightData;
+}
+
+app.post("/log-weight", (req, res) => {
+  console.log(req.body);
+  let userName = req.body.userName;
+  let userWeight = req.body.weight;
+  // let userWeight = Number(weight);
+  console.log(userName);
+  console.log(userWeight);
+
+  const date = new Date().toISOString().split('T')[0];
+
+  if (isNaN(userWeight) || userWeight <= 0) {
+    console.log(userWeight);
+    res.status(400).send({ error: "Invalid weight" });
+  } else {
+    let weightData = saveWeightData(userName, date, userWeight);
+    res.status(201).json(weightData[userName]["weightData"]);
+  }
+});
+
+app.get("/log-weight", (req, res) => {
+  const { userName } = req.query;
+  console.log(userName);
+
+  let weightData = fs.readFileSync('weight-data.json', 'utf8');
+  weightData = JSON.parse(weightData);
+  if (!weightData[userName]) {
+    console.log("User not found");
+    res.status(404).send({ error: "User not found" });
+  } else {
+    console.log("User found");
+    res.status(201).json(weightData[userName]["weightData"]);
+  }
+});
+
+app.get("/user", (req, res) => {
+  const { userName } = req.query;
+  console.log(userName);
+
+  let weightData = fs.readFileSync('weight-data.json', 'utf8');
+  
+  if (weightData === "") {
+    weightData = {};
+  } else {
+    weightData = JSON.parse(weightData);
+  }
+  if (!weightData[userName]) {
+    weightData[userName] = {'weightData': []};
+    fs.writeFileSync('weight-data.json', JSON.stringify(weightData));
+    res.status(201).json({ message: "New" });
+  } else {
+    res.status(201).json({ message: "Existing" });
+  }
 });
 
 // Start the server
